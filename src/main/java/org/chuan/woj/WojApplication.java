ac5dd8af64209752e3e35ad04b0800da1fa5cac2
chuan <3153917921@qq.com>
2024-08-13 22:06:02 +0800

    初始化项目

---
+++ src/main/java/org/chuan/woj/WojApplication.java
@@ -0,0 +1,13 @@
+package org.chuan.woj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/chuan/woj/WojApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chuan.woj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
