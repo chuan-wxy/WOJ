0d9d55027bb10f88ac746a5ff06c3aa76a14fe6b
chuan <3153917921@qq.com>
2024-09-09 12:48:12 +0800

    MyBatisPlus配置

---
+++ src/main/java/org/chuan/woj/config/MyBatisPlusConfig.java
@@ -0,0 +1,33 @@
+package org.chuan.woj.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis Plus 配置
+ *
+ * @Author: chuan-wxy
+ * @Date: 2024/9/9 12:45
+ * @Description:
+ */
+@Configuration
+@MapperScan("org.chuan.woj.mapper")
+public class MyBatisPlusConfig {
+
+    /**
+     * 拦截器配置
+     *
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
