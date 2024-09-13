d3b1b736e586496c046472ae5b325fc7544beb1b
chuan <3153917921@qq.com>
2024-09-13 14:55:52 +0800

    进程信息类

---
+++ src/main/java/org/chuan/woj/pojo/ExecuteMessage.java
@@ -0,0 +1,14 @@
+package org.chuan.woj.pojo;
+
+import lombok.Data;
+
+@Data
+public class ExecuteMessage {
+    private Integer exitValue;
+
+    private String message;
+
+    private String errorMessage;
+
+    private Long time;
+}
