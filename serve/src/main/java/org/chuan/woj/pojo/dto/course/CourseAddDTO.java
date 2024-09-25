3112c7bca698aa3ee478dd60cbf3635341d937c7
chuan <3153917921@qq.com>
2024-09-25 17:46:09 +0800

    新增课程DTO

---
+++ serve/src/main/java/org/chuan/woj/pojo/dto/course/CourseAddDTO.java
@@ -0,0 +1,32 @@
+package org.chuan.woj.pojo.dto.course;
+
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * @Author: chuan-wxy
+ * @Date: 2024/9/25 17:06
+ * @Description:
+ */
+@Data
+public class CourseAddDTO implements Serializable{
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 图像
+     */
+    private String avatar;
+
+    private static final long serialVersionUID = 1L;
+
+}
+
