2b43b69ffb1439bbf00b977a1670bb002a827229
chuan <3153917921@qq.com>
2024-09-29 18:32:12 +0800

    新增uploadCourseAvatar接口

--- serve/src/main/java/org/chuan/woj/controller/file/FileController.java
+++ serve/src/main/java/org/chuan/woj/controller/file/FileController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletOutputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.chuan.woj.annotation.AuthCheck;
 import org.chuan.woj.common.BaseResponse;
+import org.chuan.woj.exception.StatusSystemErrorException;
 import org.chuan.woj.utils.ResultUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -30,6 +31,9 @@ public class FileController {
     @Value("${path.code.judgecase-path}")
     String judgeCasePath;
 
+    @Value("${path.avatar.course-avatar-path}")
+    String courseAvatarPath;
+
     @PostMapping("/upload-avatar")
     public String uploadAvatar() {
         return null;
@@ -49,5 +53,41 @@ public class FileController {
             return ResultUtils.error("保存失败");
         }
         return ResultUtils.success("保存成功");
+    }
+
+    /**
+     * 上传课程头像接口
+     * @param file
+     * @param name
+     * @return
+     * @throws StatusSystemErrorException
+     */
+
+    @PostMapping("/upload-course-avatar")
+    public BaseResponse<String> uploadCourseAvatar(MultipartFile file, String name) throws StatusSystemErrorException {
+        String originFileName = file.getOriginalFilename();
+        String dirPath = courseAvatarPath + File.separator + name;
+
+        File dir = new File(dirPath);
+
+        if (!dir.exists()) {
+            boolean success = dir.mkdirs();
+            if (!success) {
+                log.error("无法创建目录: {}", dirPath);
+                throw new StatusSystemErrorException("目录创建失败");
+            }
+        }
+
+        String filePath = dirPath + File.separator + originFileName;
+        File courseAvatar = new File(filePath);
+
+        try {
+            file.transferTo(courseAvatar);
+            log.info("课程图片已成功保存至: {}", filePath);
+            return ResultUtils.success("保存成功");
+        } catch (IOException e) {
+            log.error("文件保存失败: {}", e.getMessage());
+            return ResultUtils.error("保存失败，错误信息: " + e.getMessage());
+        }
     }
 }
