cb1dca65c4f02ce655541b1d5a1161c01663c02c
chuan <3153917921@qq.com>
2024-10-01 10:49:35 +0800

    【弃用】Tag注解

--- serve/src/main/java/org/chuan/woj/controller/admin/AdminController.java
+++ serve/src/main/java/org/chuan/woj/controller/admin/AdminController.java
@@ -25,7 +25,6 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/amdin")
-@Tag(name = "AdminController")
 public class AdminController {
     @Autowired
     UserService userService;
