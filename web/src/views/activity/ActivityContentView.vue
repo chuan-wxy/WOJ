a05660b624701a21cc997f5d995c9776e0cd4e35
chuan <3153917921@qq.com>
2024-10-03 19:14:53 +0800

    【新增】活动详情页面

--- web/src/views/activity/ActivityContentView.vue
+++ web/src/views/activity/ActivityContentView.vue
@@ -1,21 +1,9 @@
 <template>
   <div id="problem-content">
     <div class="panel-body">
-      <h2>{{ problemData.title }}</h2>
+      <h2>{{ activityData.title }}</h2>
       <div class="content">
-        <Viewer :value="problemData.description" />
-      </div>
-      <br />
-      <div>
-        <CodeEditor ref="codeEditor" />
-      </div>
-      <div>
-        <a-button @click="submit">提交</a-button>
-      </div>
-      <div :class="[isHide == true ? 'cardIsHide' : 'cardNoHide']">
-        代码提交状态：
-        <a-spin :class="[isState == true ? 'isHide' : 'noHide']" />
-        {{ message }}
+        <Viewer :value="activityData.description" />
       </div>
     </div>
   </div>
@@ -24,77 +12,33 @@
 <script setup lang="ts">
 import { onBeforeMount, ref } from "vue";
 import { useRoute } from "vue-router";
-import { ProblemControllerService } from "../../../generated/services/ProblemControllerService";
 import { ElMessage } from "element-plus";
-import CodeEditor from "@/components/CodeEditor.vue";
-import { ProblemSubmitControllerService } from "../../../generated/services/ProblemSubmitControllerService";
+import { ActivityControllerService } from "../../../generated/services/ActivityControllerService";
+import { Viewer } from "@bytemd/vue-next";
 
 const route = useRoute();
-const isState = ref(true);
-const isHide = ref(true);
-const message = ref("");
-const codeEditor = ref();
 
-const problemData = ref({
+const activityData = ref({
   id: 0,
-  problemId: 0,
-  title: 0,
-  author: "",
-  tagList: [],
+  title: "",
   description: "",
-  input: "",
-  output: "",
-  source: "",
-  difficulty: 0,
-  auth: 0,
 });
-
-const submitData = ref({
-  language: "c++",
-  code: "",
-  pid: 0,
-});
-
-const submit = async () => {
-  if (
-    codeEditor.value.codeEditorData === null ||
-    codeEditor.value.codeEditorData === ""
-  ) {
-    ElMessage.error("代码不能为空");
-    return;
-  }
-  isState.value = false;
-  isHide.value = false;
-  //先清空以前的数据
-  message.value = "";
-  submitData.value.code = codeEditor.value.codeEditorData;
-  const result = await ProblemSubmitControllerService.doSubmit(
-    submitData.value
-  );
-  isState.value = true;
-  if (result.code === 0) {
-    message.value = result.message ?? "";
-    ElMessage.success("添加成功");
-  } else {
-    ElMessage.error("添加失败：" + result.message);
-  }
-};
 
 const loadData = async () => {
-  const id = route.query.id;
+  const id = Number(route.query.id);
   if (!id) {
     return;
   }
-  const res = await ProblemControllerService.getProblem(id as any);
+  const res = await ActivityControllerService.getActivity(id);
   if (res.code === 0) {
-    problemData.value = res.data as any;
+    activityData.value = res.data as any;
+    console.log(res);
   } else {
     ElMessage.error("加载失败：" + res.message);
   }
 };
 onBeforeMount(() => {
   loadData();
-  submitData.value.pid = route.query.id as any;
 });
 </script>
 
