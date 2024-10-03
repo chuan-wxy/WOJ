8c90e79501fcc8ddda08aa015516954c83ec8498
chuan <3153917921@qq.com>
2024-10-03 19:17:57 +0800

    【新增】活动接口

---
+++ serve/src/main/java/org/chuan/woj/controller/oj/ActivityController.java
@@ -0,0 +1,54 @@
+package org.chuan.woj.controller.oj;
+
+import lombok.extern.slf4j.Slf4j;
+import org.chuan.woj.common.BaseResponse;
+import org.chuan.woj.exception.StatusFailException;
+import org.chuan.woj.pojo.dto.activity.ActivityAddDTO;
+import org.chuan.woj.pojo.vo.activity.ActivityContentVO;
+import org.chuan.woj.pojo.vo.activity.ActivityTitleVO;
+import org.chuan.woj.service.activity.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author: chuan-wxy
+ * @Date: 2024/10/2 20:26
+ * @Description:
+ */
+@Slf4j
+@RestController
+@RequestMapping("/activity")
+public class ActivityController {
+    @Autowired
+    ActivityService activityService;
+
+    /**
+     * 新增活动
+     * @param activityAddDTO
+     * @return
+     */
+    @PostMapping("/add")
+    public BaseResponse<String> addActivity(@RequestBody ActivityAddDTO activityAddDTO) throws StatusFailException {
+        return activityService.addActivity(activityAddDTO);
+    }
+
+    /**
+     * 获取活动标题列表
+     * @return
+     */
+    @GetMapping("/get-activity-list")
+    public BaseResponse<List<ActivityTitleVO>> getActivityList() {
+        return activityService.getActivityList();
+    }
+
+    /**
+     * 获取单个活动文章
+     * @return
+     */
+    @GetMapping("/get-activity")
+    public BaseResponse<ActivityContentVO> getActivity(@RequestParam Integer id) {
+        return activityService.getActivity(id);
+    }
+}
