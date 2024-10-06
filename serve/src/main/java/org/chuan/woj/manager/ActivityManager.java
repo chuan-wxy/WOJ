2770c26f525133239507492679b45211fafa03a0
chuan <3153917921@qq.com>
2024-10-06 21:28:24 +0800

    【新增】添加公告请求体的校验

--- serve/src/main/java/org/chuan/woj/manager/ActivityManager.java
+++ serve/src/main/java/org/chuan/woj/manager/ActivityManager.java
@@ -4,8 +4,11 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.chuan.woj.exception.StatusFailException;
 import org.chuan.woj.mapper.ActivityMapper;
+import org.chuan.woj.mapper.AnnouncementMapper;
 import org.chuan.woj.pojo.dto.activity.ActivityAddDTO;
+import org.chuan.woj.pojo.dto.announcement.AnnouncementAddDTO;
 import org.chuan.woj.pojo.entity.Activity;
+import org.chuan.woj.pojo.entity.Announcement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,8 +20,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class ActivityManager {
+
     @Autowired
     ActivityMapper activityMapper;
+
+    @Autowired
+    AnnouncementMapper announcementMapper;
     public void activityAddvalidate(ActivityAddDTO activityAddDTO) throws StatusFailException {
         String title = activityAddDTO.getTitle();
 
@@ -32,6 +39,24 @@ public class ActivityManager {
         Activity activity = activityMapper.selectOne(queryWrapper);
 
         if(activity != null) {
+            throw new StatusFailException("标题重复");
+        }
+
+    }
+
+    public void announcementAddvalidate(AnnouncementAddDTO announcementAddDTO) throws StatusFailException {
+        String title = announcementAddDTO.getTitle();
+
+        if(title == null || title.isBlank()) {
+            throw new StatusFailException("标题不能为空");
+        }
+
+        QueryWrapper<Announcement> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("title",title);
+
+        Announcement announcement = announcementMapper.selectOne(queryWrapper);
+
+        if(announcement != null) {
             throw new StatusFailException("标题重复");
         }
 
